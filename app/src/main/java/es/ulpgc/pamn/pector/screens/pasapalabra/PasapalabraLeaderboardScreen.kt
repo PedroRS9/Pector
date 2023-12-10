0c9d22d9d84b40a6739bfd9a0df8fe3c7ea3f555
PedroRS9 <pedroglv98@gmail.com>
2023-12-10 19:16:42 +0000

    Pasapalabra Title in Leaderboard

--- app/src/main/java/es/ulpgc/pamn/pector/screens/pasapalabra/PasapalabraLeaderboardScreen.kt
+++ app/src/main/java/es/ulpgc/pamn/pector/screens/pasapalabra/PasapalabraLeaderboardScreen.kt
@@ -3,9 +3,11 @@ package es.ulpgc.pamn.pector.screens.pasapalabra
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -14,6 +16,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavController
@@ -59,6 +62,12 @@ fun BodyContent(
         LaunchedEffect(Unit){
             downloadLeaderboard()
         }
+        Text(
+            text = "Pasapalabra",
+            style = MaterialTheme.typography.bodyLarge,
+            fontSize = 40.sp,
+            modifier = Modifier.padding(20.dp)
+        )
         PectorButton(
             text = "JUGAR",
             onClick = { navController.navigate(AppScreens.PasapalabraScreen.route) },
